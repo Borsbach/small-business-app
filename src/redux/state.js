90452fe4d023ed94eb074d1e6e9c61a49770193f
Alex Borsbach <alexborsbach@alexs-mbp.home>
2019-10-23 20:34:49 -0500

    upload afer 2nd class day

---
+++ src/redux/state.js
@@ -0,0 +1,24 @@
+export default {
+  listings: [
+    {
+    "id": 1,
+      "Name":"MasFajitas",
+      "Description":"Informal outpost of a small chain known for its menu of Mexican favorites & traditional cocktails.",
+      "Hours":"11AM-10PM",
+      "Address":"1700 E Palm Valley Blvd, Round Rock, TX 78664"
+  },
+  {
+    "id": 2,
+      "Name":"Ajo",
+      "Description":"Casual, contemporary restaurant featuring New American dishes featuring local ingredients.",
+      "Hours":"11AM-10PM",
+      "Address":"121 E Main St, Round Rock, TX 78664"
+  },
+  {
+    "id": 3,
+      "Name":"Gino's Italian Restaurant and Pizza",
+      "Description":"Family-run spot with Italian standards like pasta, pizza & subs in a casual strip-mall outpost.",
+      "Hours":"11AM-2:30PM and 5-9:30PM",
+      "Address":"1701 S Mays St, Round Rock, TX 78664"
+  }]
+}
